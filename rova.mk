8a96eaa6943640c3c2338f564912e6c6ad1c9d23
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-04-14 19:21:09 +0800

    rova-common: overlay: Kang power_profile from tiare

--- rova.mk
+++ rova.mk
@@ -7,6 +7,9 @@
 # Inherit from msm8937-common
 $(call inherit-product, device/xiaomi/msm8937-common/msm8937.mk)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
