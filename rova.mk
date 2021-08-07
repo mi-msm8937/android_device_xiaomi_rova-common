b1a423c0f0bb4db7f4082f2c434897aaff91e0be
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-07 23:07:30 +0800

    rova-common: audio: Import audio_platform_info from LA.UM.8.6.2.r1-08100-89xx.0

--- rova.mk
+++ rova.mk
@@ -7,6 +7,10 @@
 # Inherit from msm8937-common
 $(call inherit-product, device/xiaomi/msm8937-common/msm8937.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
