f1e8d077550fbe568a7953263374fbbb42f79a97
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-08 21:50:26 +0800

    rova-common: Use sysctl DT2W control node

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -9,6 +9,9 @@ include device/xiaomi/msm8937-common/BoardConfigCommon.mk
 
 DEVICE_COMMON_PATH := device/xiaomi/rova-common
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/proc/sys/dev/dt2w"
+
 # SELinux
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_COMMON_PATH)/sepolicy/vendor
 
