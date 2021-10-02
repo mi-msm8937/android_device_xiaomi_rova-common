3dce4273ef13cbf339aa28d75737e6100e75e785
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-10-02 16:35:35 +0800

    rova-common: Import blob extractors
    Removed some variables as this is device specific common tree.

---
+++ setup-makefiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE_COMMON=msm8937-common
+
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
